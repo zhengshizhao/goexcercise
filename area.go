986d65e2a74da2f0cee581f1a6108b1304c8f7b5
ZhengshiZhao <zhao.zhengshi@gmail.com>
2017-09-15 21:35:39 -0400

    more exercise

---
+++ area.go
@@ -0,0 +1,45 @@
+// HANDS ON 1
+// create a type square
+// create a type circle
+// attach a method to each that calculates area and returns it
+// create a type shape which defines an interface as anything which has the area method
+// create a func info which takes type shape and then prints the area
+// create a value of type square
+// create a value of type circle
+// use func info to print the area of square
+// use func info to print the area of circle
+
+package main
+
+import ( "fmt"
+          "math" 
+        )
+type square struct{
+	length float32
+	width float32
+
+}
+type circle struct{
+	radius float32
+
+}
+type shape interface {
+	area() float32
+}
+
+func (s square) area() float32 {
+	return s.length * s.width
+}
+
+func (c circle) area() float32{
+	return math.Pi * c.radius * c.radius
+}
+func info(sh shape) {
+	fmt.Println(sh.area())
+}
+func main() {
+	square1 := square{3.5,5}
+	circle1 := circle{7.8}
+    info(square1)
+    info(circle1)
+}
---
+++ speak.go
@@ -0,0 +1,46 @@
+// create a struct that holds person fields
+// create a struct that holds secret agent fields and embeds person type
+// attach a method to person: pSpeak
+// attach a method to secret agent: saSpeak
+// create a variable of type person
+// create a variable of type secret agent
+// print a field from person
+// run pSpeak attached to the variable of type person
+// print a field from secret agent
+// run saSpeak attached to the variable of type secret agent
+// run pSpeak attached to the variable of type secret agent
+
+
+
+package main
+
+import ( "fmt"
+        )
+
+type person struct {
+	fName string
+	lName string
+}
+
+type secretagent struct {
+	person
+	secretAgent bool
+}
+
+func(p person) pSpeak () string {
+	return "Hi, I am " + p.fName
+}
+func(sa secretagent) saSpeak () string {
+	return fmt.Sprintln("Hi, I know " + sa.fName)
+}
+
+func main (){
+	person1 := person {"Jany", "Kim"}
+	fmt.Println(person1.lName)
+    
+    sa1 := secretagent{person{"Tom","Smith"}, true}
+    fmt.Println(sa1.person.lName)
+    fmt.Println(person1.pSpeak())
+    fmt.Println(sa1.saSpeak())
+
+}
